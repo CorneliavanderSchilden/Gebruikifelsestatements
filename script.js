c5a6ab976f90bda454bddf335a94d21ceb9a544d
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-08 10:55:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const age = 20;
+
+if (age > 17) {
+    console.log("Je bent ouder dan 18 jaar. Welkom in onze kroeg");
+}
+else if (age < 18) {
+    console.log("Je bent jonger dan 18 jaar. Helaas mogen we je niet binnen laten");
+}
+
+const isFemale = false;
+
+if (isFemale == true) {
+    console.log("Welkom bij ladies night");
+}
+
+else {
+    console.log("Ladies only vanavond. Tot een andere keer");
+}
+
+const driverStatus = "Karin";
+
+if (driverStatus == "Bob") {
+    console.log("Hoi Bob. Have a safe drive home!")
+}
+else {
+    console.log("Glaasje op laat je rijden")
+}
